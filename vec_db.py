bfce59a6818179b1ab8ae2ad82e8738bb1f1e3f0
daniel <daniel.atallah01@gmail.com>
2024-12-13 01:04:18 +0200

    edit params

--- vec_db.py
+++ vec_db.py
@@ -6,12 +6,12 @@ from IVF import *
 DB_SEED_NUMBER = 42
 ELEMENT_SIZE = np.dtype(np.float32).itemsize
 DIMENSION = 70
-NCLUSTERS = 500
-NPROBS = 30
+NCLUSTERS = 5000
+NPROBS = 200
 BATCH_SIZE = 10000
 
 class VecDB:
-    def __init__(self, database_file_path = "saved_db.dat", index_file_path = "Databases/1000000", new_db = True, db_size = None) -> None:
+    def __init__(self, database_file_path = "saved_db.dat", index_file_path = "Databases/10000000", new_db = True, db_size = None) -> None:
         self.db_path = database_file_path
         self.index_path = index_file_path
         self.db_size= db_size
