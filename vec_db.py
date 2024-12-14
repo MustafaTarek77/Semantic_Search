05bc69a2a61cc783ed4d65980cc35d1e4be8e203
daniel <daniel.atallah01@gmail.com>
2024-12-14 02:15:30 +0200

    edit params

--- vec_db.py
+++ vec_db.py
@@ -35,8 +35,8 @@ class VecDB:
             self.BATCH_SIZE = 30000
         elif self.db_size == 20*10**6:
             self.NCLUSTERS = 10000
-            self.NPROBS = 50
-            self.BATCH_SIZE = 50000
+            self.NPROBS = 110
+            self.BATCH_SIZE = 30000
             
         if new_db:
             if db_size is None:
