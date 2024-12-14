e8b321fe88d8d60607de3b559a5c0f50c7c8bb06
daniel <daniel.atallah01@gmail.com>
2024-12-14 03:26:10 +0200

    edit params

--- IVF.py
+++ IVF.py
@@ -48,7 +48,7 @@ class IVF:
         
     def retrieve(self, query, top_k, index_path=None, batch_size=None):
         if self.data_size == 20*10**6 or self.data_size == 10**6:
-            centroids_generator = read_khra_centroids_file(os.path.join(index_path, self.centroids_file_path), self.dimension,100)
+            centroids_generator = read_khra_centroids_file(os.path.join(index_path, self.centroids_file_path), self.dimension,5000)
             self.centroids = np.vstack([batch for batch in centroids_generator])    
             # self.centroids= read_whole_centroids_file(os.path.join(index_path, self.centroids_file_path), self.dimension)  
         else:
